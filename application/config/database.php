376cd49a2c4b2123c152a97809c34b8c042a07b2
Martin Drapeau <martindrapeau@gmail.com>
2013-12-17 20:13:12 -0500

    Revert unecessary change to db default not to use it. DB is not loaded anyhow.

--- application/config/database.php
+++ application/config/database.php
@@ -61,7 +61,7 @@ $db['default']['cachedir'] = '';
 $db['default']['char_set'] = 'utf8';
 $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = FALSE;
+$db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
 
