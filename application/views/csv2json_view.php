6d6046cce6934d90dc079841eb842c7e29e29195
Martin Drapeau <martindrapeau@gmail.com>
2013-12-20 21:30:21 -0500

    Styling.

--- application/views/csv2json_view.php
+++ application/views/csv2json_view.php
@@ -10,6 +10,7 @@
 		<title>CSV to JSON</title>
 		
 		<script src="/js/jquery-2.0.3.min.js" type="text/javascript"></script>
+		<script src="/js/json/json2.js" type="text/javascript"></script>
 		<script src="/js/jQuery-File-Upload/js/vendor/jquery.ui.widget.js"></script>
 		<script src="/js/jQuery-File-Upload/js/jquery.iframe-transport.js"></script>
 		<script src="/js/jQuery-File-Upload/js/jquery.fileupload.js"></script>
@@ -20,43 +21,52 @@
 		<script src="/js/bootstrap/js/bootstrap.min.js"></script>
 		<link href="/js/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" charset="utf-8" />
 		
-		<link href="/css/csv2json.css" rel="stylesheet" type="text/css" charset="utf-8" />
+		<link href="/css/csvjson.css" rel="stylesheet" type="text/css" charset="utf-8" />
+		
 		<script src="/js/csv2json.js"></script>
 		
 	</head>
 	<body>
+		<nav class="navbar navbar-inverse" role="navigation">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="/">CSVJSON</a>
+			</div>
+			<div class="collapse navbar-collapse" id="navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a>CSV to JSON</a></li>
+					<li><a href="/json-validator">JSON Validator</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="/about">About</a></li>
+				</ul>
+			</div>
+		</nav>
 		<div class="container">
 			<div class="row">
-				<div class="col-md-12">
-					<h1>CSV to JSON</h1>
+				<div class="header col-md-12">
 					<p>Convert your CSV formatted data to JSON.</p>
 				</div>
 			</div>
 			
 			<div class="row">
-				<div class="form-group col-md-6">
-					<label>Upload a file</label>
-					<span class="btn btn-default fileinput-button form-control">
-						<label>
-							<i class="glyphicon glyphicon-plus"></i>
-							<span>Select a file...</span>
-						</label>
-						<input id="fileupload" type="file" name="file" />
-					</span>
-				</div>
-				<div class="form-group col-md-6">
-					<label>Field separator</label>
-					<div class="form-control">
-						<label class="radio-inline"><input type="radio" id="auto-detect" name="separator" value="auto" checked="checked" />Auto-detect</label>
-						<label class="radio-inline"><input type="radio" id="comma" name="separator" value="comma" />Comma</label>
-						<label class="radio-inline"><input type="radio" id="semi-colon" name="separator" value="semiColon" />Semi-colon</label>
-						<label class="radio-inline"><input type="radio" id="tab" name="separator" value="tab" />Tab</label>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-6">
+				<div class="csv col-md-6">
 					<div class="form-group">
+						<label>Upload a file</label>
+						<span class="btn btn-default fileinput-button form-control">
+							<label>
+								<i class="glyphicon glyphicon-plus"></i>
+								<span>Select a file...</span>
+							</label>
+							<input id="fileupload" type="file" name="file" />
+						</span>
+					</div>
+					<div class="form-group code-group">
 						<label>Or paste your CSV here</label>
 						<textarea id="csv" class="form-control"><?="a,b,c\n1,2,3"?></textarea>
 						<a class="clear" href="#" title="Clear"><i class="glyphicon glyphicon-remove"></i></a>
@@ -64,8 +74,17 @@
 					<button id="convert" type="submit" class="btn btn-primary">Convert</button>
 				</div>
 				
-				<div class="col-md-6">
+				<div class="json col-md-6">
 					<div class="form-group">
+						<label>Field separator</label>
+						<div class="form-control">
+							<label class="radio-inline"><input type="radio" id="auto-detect" name="separator" value="auto" checked="checked" />Auto-detect</label>
+							<label class="radio-inline"><input type="radio" id="comma" name="separator" value="comma" />Comma</label>
+							<label class="radio-inline"><input type="radio" id="semi-colon" name="separator" value="semiColon" />Semi-colon</label>
+							<label class="radio-inline"><input type="radio" id="tab" name="separator" value="tab" />Tab</label>
+						</div>
+					</div>
+					<div class="form-group code-group">
 						<label>JSON</label>
 						<textarea id="json" class="form-control"></textarea>
 						<a class="clear" href="#" title="Clear"><i class="glyphicon glyphicon-remove"></i></a>
