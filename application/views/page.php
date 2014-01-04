0b53134bac60f80e142644dbe400b7fb0cd286d2
Martin Drapeau <martindrapeau@gmail.com>
2014-01-03 22:13:35 -0500

    .

--- application/views/page.php
+++ application/views/page.php
@@ -58,7 +58,7 @@
 		<footer class="navbar">
 			<div class="container">
 				<p class="pull-left">&copy; 2014 <a href="http://martindrapeau.tumblr.com/">Martin Drapeau</a>. Built in Montréal.</p>
-				<p class="pull-right"><a href="https://github.com/martindrapeau/CSVJSON">Code is on Github</a></p>
+				<p class="pull-right"><a href="https://github.com/martindrapeau/CSVJSON">Code on Github</a></p>
 			</div>
 		</footer>
 	</body>
