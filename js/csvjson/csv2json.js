da2e5f3662dbc5fed7175581da2e74fe8af6b7c7
Martin Drapeau <martindrapeau@gmail.com>
2015-06-01 21:39:20 -0400

    Replaced the CSV parser to support commas inside quotes

--- js/csvjson/csv2json.js
+++ js/csvjson/csv2json.js
@@ -32,6 +32,90 @@
 		});
 		return sepMax ? sepMax : undefined;
 	}
+
+	// source: http://stackoverflow.com/a/1293163/2343
+    function CSVtoArray( strData, strDelimiter ){
+        // Check to see if the delimiter is defined. If not,
+        // then default to comma.
+        strDelimiter = (strDelimiter || ",");
+
+        // Create a regular expression to parse the CSV values.
+        var objPattern = new RegExp(
+            (
+                // Delimiters.
+                "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+                // Quoted fields.
+                "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+                // Standard fields.
+                "([^\"\\" + strDelimiter + "\\r\\n]*))"
+            ),
+            "gi"
+            );
+
+
+        // Create an array to hold our data. Give the array
+        // a default empty first row.
+        var arrData = [[]];
+
+        // Create an array to hold our individual pattern
+        // matching groups.
+        var arrMatches = null;
+
+
+        // Keep looping over the regular expression matches
+        // until we can no longer find a match.
+        while (arrMatches = objPattern.exec( strData )){
+
+            // Get the delimiter that was found.
+            var strMatchedDelimiter = arrMatches[ 1 ];
+
+            // Check to see if the given delimiter has a length
+            // (is not the start of string) and if it matches
+            // field delimiter. If id does not, then we know
+            // that this delimiter is a row delimiter.
+            if (
+                strMatchedDelimiter.length &&
+                strMatchedDelimiter !== strDelimiter
+                ){
+
+                // Since we have reached a new row of data,
+                // add an empty row to our data array.
+                arrData.push( [] );
+
+            }
+
+            var strMatchedValue;
+
+            // Now that we have our delimiter out of the way,
+            // let's check to see which kind of value we
+            // captured (quoted or unquoted).
+            if (arrMatches[ 2 ]){
+
+                // We found a quoted value. When we capture
+                // this value, unescape any double quotes.
+                strMatchedValue = arrMatches[ 2 ].replace(
+                    new RegExp( "\"\"", "g" ),
+                    "\""
+                    );
+
+            } else {
+
+                // We found a non-quoted value.
+                strMatchedValue = arrMatches[ 3 ];
+
+            }
+
+
+            // Now that we have our value string, let's add
+            // it to the data array.
+            arrData[ arrData.length - 1 ].push( strMatchedValue );
+        }
+
+        // Return the parsed data.
+        return( arrData );
+    }
 	
 	function convert(csv, options) {
 		options || (options = {});
@@ -39,24 +123,21 @@
 		
 		var separator = options.separator || detectSeparator(csv);
 		if (!separator) throw errorDetectingSeparator;
-		
-		var lines = _.lines(csv);
-		if (lines.length == 0) throw errorEmpty;
-		
-		// Extract and clean
-		var keys = _.words(lines.shift(), separator);
+
+		var a = CSVtoArray(csv, separator);
+		if (!a) throw errorEmpty;
+
+		var keys = a.shift();
 		if (keys.length == 0) throw errorEmptyHeader;
 		keys = _.map(keys, function(key) {
 			return _(key).chain().trim().trim('"').value();
 		});
 		
-		// Extra data
 		var	json = [];
-		for (var l = 0; l < lines.length; l++) {
+		for (var l = 0; l < a.length; l++) {
 			var row = {};
-			var items = _.words(lines[l], separator);
 			for (var i = 0; i < keys.length; i++) {
-				var value = _(items[i]).chain().trim().trim('"').value(),
+				var value = _(a[l][i]).chain().trim().trim('"').value(),
 					number = value - 0;
 				row[keys[i]] = isNaN(number) ? value : number;
 			}
