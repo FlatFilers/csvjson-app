7e0c7fb0db2226a7f7ee7ea0e7337b78768c9917
Martin Drapeau <martindrapeau@gmail.com>
2015-10-09 15:05:13 -0400

    300x250 ad

--- application/views/csv2json_view.php
+++ application/views/csv2json_view.php
@@ -83,11 +83,11 @@ Nine Miles from the White City, 2013, -
 		</div>
 		<div class="col-md-4">
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- 320x100 -->
+<!-- 300x250 -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:320px;height:100px"
+     style="display:inline-block;width:300px;height:250px"
      data-ad-client="ca-pub-8893443121967263"
-     data-ad-slot="7472382034"></ins>
+     data-ad-slot="8890239630"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
