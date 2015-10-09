1fc14cb20f15f18f89814c6ca4b102ee0cefbf94
Martin Drapeau <martindrapeau@gmail.com>
2015-10-08 22:12:30 -0400

    Attempt to fix GA error: Bad Event Tracking Code The Landing Pages report has a (not set) entry. Verify that tracking code for property www.csvjson.com sends a _trackPageview hit and that it does this before sending any events.

--- application/views/page.php
+++ application/views/page.php
@@ -32,8 +32,10 @@
 		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		  ga('create', 'UA-46942708-1', 'csvjson.com');
+
+		  ga('create', 'UA-46942708-1', 'auto');
 		  ga('send', 'pageview');
+
 		</script>
 	</head>
 	<body>
