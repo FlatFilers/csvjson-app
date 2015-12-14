31c5165f9c073b04c955ad2195696aafed4f4843
Martin Drapeau <martindrapeau@gmail.com>
2015-12-13 21:59:36 -0500

    Replaced adsense by Ludo's Quest.

---
+++ application/views/adsense.php
@@ -0,0 +1,10 @@
+<h4>&nbsp;</h4>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 320x100 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:320px;height:100px"
+     data-ad-client="ca-pub-8893443121967263"
+     data-ad-slot="7472382034"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
---
+++ application/views/ludo.php
@@ -0,0 +1,25 @@
+<style>
+	.ludo p {
+		background-color: #333;
+	    width: 300px;
+	    margin: 0;
+	    padding: 5px;
+	    text-align: center;
+	    color: #fff;
+	    font-family: cursive;
+	    font-size: 20px;
+	}
+	.ludo a:hover,
+	.ludo a:active,
+	.ludo a:focus {
+		text-decoration: none;
+		color: #fff;
+	}
+</style>
+<div class="ludo">
+	<a href="http://www.ludosquest.com" target="_blank">
+		<p>Take a break from coding. Play Ludo's Quest for 5 minutes.</p>
+		<img src="http://www.ludosquest.com/site/img/screen<?=rand(1,23)?>-small.png" alt="Ludo's Quest" />
+		<p>www.ludosquest.com</p>
+	</a>
+</div>
