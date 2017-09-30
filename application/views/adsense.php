825b468b5a44d10055333d18b0a470c0195a9aa6
Martin Drapeau <martindrapeau@gmail.com>
2017-09-30 06:54:52 -0400

    AdSense

--- application/views/adsense.php
+++ application/views/adsense.php
@@ -1,10 +1,10 @@
 <h4>&nbsp;</h4>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- CSVJSON -->
+<!-- bottom right -->
 <ins class="adsbygoogle"
      style="display:inline-block;width:336px;height:280px"
      data-ad-client="ca-pub-8893443121967263"
-     data-ad-slot="9438324528"></ins>
+     data-ad-slot="8309983216"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
--- application/views/page.php
+++ application/views/page.php
@@ -32,10 +32,15 @@
 		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
 		  ga('create', 'UA-46942708-1', 'auto');
 		  ga('send', 'pageview');
-
+		</script>
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+		  (adsbygoogle = window.adsbygoogle || []).push({
+		    google_ad_client: "ca-pub-8893443121967263",
+		    enable_page_level_ads: true
+		  });
 		</script>
 	</head>
 	<body>
