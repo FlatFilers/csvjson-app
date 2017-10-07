d16a8a0217b3bde59b1d7bf518e8b18c7e582e80
Martin Drapeau <martindrapeau@gmail.com>
2017-10-07 04:13:03 -0400

    Styling of carbon ads

--- application/views/carbonads.php
+++ application/views/carbonads.php
@@ -1,4 +1,28 @@
 <?php if (ENVIRONMENT == 'production'): ?>
-  <div id="carbonads"></div>
-  <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=csvjsoncom" id="_carbonads_js"></script>
+<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=csvjsoncom" id="_carbonads_js"></script>
+<style>
+  #carbonads {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    box-sizing: border-box;
+    float: right;
+    background-color: rgba(0,0,0, 0.1);
+    padding: 1.5rem;
+    width: 32rem;
+    text-align: left !important;
+    /* font-size: 1.1rem; */
+    margin: 0;
+    min-height: 1.3rem;
+    z-index: 1000;
+  }
+  #carbonads .carbon-img {
+    float: left;
+    width: 14.5rem;
+  }
+  #carbonads .carbon-poweredby {
+    margin-top: 1rem;
+    display: block;
+  }
+</style>
 <?php endif; ?>
