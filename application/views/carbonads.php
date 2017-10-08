3922389e1a9196f131b99d8ce7e652a47788de20
Martin Drapeau <martindrapeau@gmail.com>
2017-10-07 21:30:09 -0400

    Responsive carbon ad - when viewport is too small, do not keep in fixed position as it hides too much.

--- application/views/carbonads.php
+++ application/views/carbonads.php
@@ -1,28 +1,30 @@
 <?php if (ENVIRONMENT == 'production'): ?>
 <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=csvjsoncom" id="_carbonads_js"></script>
 <style>
-  #carbonads {
-    position: fixed;
-    right: 0;
-    bottom: 0;
-    box-sizing: border-box;
-    float: right;
-    background-color: rgba(0,0,0, 0.1);
-    padding: 1.5rem;
-    width: 32rem;
-    text-align: left !important;
-    margin: 0;
-    min-height: 1.3rem;
-    z-index: 1000;
-    border-top-left-radius: 10px;
-  }
-  #carbonads .carbon-img {
-    float: left;
-    width: 14.5rem;
-  }
-  #carbonads .carbon-poweredby {
-    margin-top: 1rem;
-    display: block;
+  @media (max-width: 767px) {
+    #carbonads {
+      position: fixed;
+      right: 0;
+      bottom: 0;
+      box-sizing: border-box;
+      float: right;
+      background-color: rgba(0,0,0, 0.1);
+      padding: 1.5rem;
+      width: 32rem;
+      text-align: left !important;
+      margin: 0;
+      min-height: 1.3rem;
+      z-index: 1000;
+      border-top-left-radius: 10px;
+    }
+    #carbonads .carbon-img {
+      float: left;
+      width: 14.5rem;
+    }
+    #carbonads .carbon-poweredby {
+      margin-top: 1rem;
+      display: block;
+    }
   }
 </style>
 <?php endif; ?>
