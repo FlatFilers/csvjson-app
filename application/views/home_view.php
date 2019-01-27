2505ef33c7cdb8e94998b9f8b5aa3a169b36b03a
martindrapeau <martindrapeau@gmail.com>
2019-01-26 21:35:20 -0500

    .

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -97,7 +97,7 @@
 			<h3>Change Log</h3>
 			<h4>2019-01-26</h4>
 			<p>
-				Improvement: Removed 65k limit on download button.</li>
+				Improvement: Removed 64k limit on download button.</li>
 			</p>
 			<h4>2018-12-26</h4>
 			<p>
