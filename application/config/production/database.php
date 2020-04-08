dcdb0b91a65295db1a352a3d305bd480a3a049c0
Robbie Trencheny <me@robbiet.us>
2020-04-08 11:44:23 -0700

    get mysql credentials out of env var

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -48,10 +48,12 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = $_ENV['MYSQL_HOSTNAME'];
-$db['default']['username'] = $_ENV['MYSQL_USERNAME'];
-$db['default']['password'] = $_ENV['MYSQL_PASSWORD'];
-$db['default']['database'] = $_ENV['MYSQL_DATABASE'];
+$url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+
+$db['default']['hostname'] = $url['host'];
+$db['default']['username'] = $url['user'];
+$db['default']['password'] = $url['pass'];
+$db['default']['database'] = substr($url['path'], 1);
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
