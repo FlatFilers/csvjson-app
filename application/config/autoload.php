c0d5766201422d0de8272284f096fc6150368516
Robbie Trencheny <me@robbiet.us>
2020-04-21 13:07:14 -0700

    chore: fix name

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('email', 'logging');
+$autoload['libraries'] = array('email', 'log');
 
 
 /*
