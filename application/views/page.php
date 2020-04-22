45fab01a6f1c967e1a543aa5e45cc1baf5c2a8ae
Robbie Trencheny <me@robbiet.us>
2020-04-22 10:10:26 -0700

    add segment

--- application/views/page.php
+++ application/views/page.php
@@ -5,12 +5,12 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<link href="/img/favicon.ico" rel="shortcut icon" type="image/x-icon">
-		
+
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		
+
 		<title><?=$title?> - CSVJSON</title>
 		<meta name="description" content="<?=$description?>">
-		
+
 		<script type="text/javascript">
 			window.APP = {
 				page: "<?=$page?>",
@@ -29,7 +29,7 @@
 				}
 			};
 		</script>
-		
+
 		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js" type="text/javascript"></script>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
 		<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" charset="utf-8" />
@@ -46,16 +46,15 @@
 			<link href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.40.0/codemirror.min.css" rel="stylesheet" type="text/css" charset="utf-8" />
 			<link href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.40.0/addon/lint/lint.min.css" rel="stylesheet" type="text/css" charset="utf-8" />
 		<?php endif; ?>
-		
+
 		<?php $this->load->view('assets'); ?>
-		
-		<!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-5B6L8X5');</script>
-		<!-- End Google Tag Manager -->
+
+		<script>
+			!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+				analytics.load("mVmXAtABgYVqPdbXw1a4Y19vcesa1cec");
+				analytics.page();
+			}}();
+	</script>
 	</head>
 	<body>
 		<header class="navbar" role="banner">
@@ -113,7 +112,7 @@
 		</header>
 
 		<?php $this->load->view($view); ?>
-		
+
 		<footer class="navbar">
 			<div class="container-fluid">
 				<p>
