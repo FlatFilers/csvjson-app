7fc12f237dd6a0f3d203e1555fc5c1f251b3539e
Alnoor Pirani <alnoor@Alnoors-MacBook-Pro.local>
2021-12-23 17:05:23 -0500

    update flatfile links again

--- application/views/page.php
+++ application/views/page.php
@@ -87,7 +87,7 @@
 					</ul>
 					<ul class="nav navbar-nav navbar-right flatfile-banner">
 						<li>
-							<a href="<?=$page == 'csv2json' ? 'https://flatfile.com/get-started?/?utm_source%3Dcsvjson-home%26utm_medium%3Dheader%26utm_content%3D%3Cid%3E%26utm_campaign%3Dq1-2022-csvjson-redesign' : 'https://flatfile.com/get-started?/?utm_source%3Dcsvjson-tools%26utm_medium%3Dheader%26utm_content%3D%3Cid%3E%26utm_campaign%3Dq1-2022-csvjson-redesign'?>"
+							<a href="<?=$page == 'csv2json' ? 'https://flatfile.com/get-started?utm_source=csvjson-home&utm_medium=header&utm_campaign=q1-2022-csvjson-redesign&utm-content={ajs_prop_ccf_id}' : 'https://flatfile.com/get-started?utm_source=csvjson-tools&utm_medium=header&utm_campaign=q1-2022-csvjson-redesign&utm-content={ajs_prop_ccf_id}'?>"
 								title="The seamless way to import, clean, & consolidate customer data">
 								<span class="image-wrapper">
 									<img src="/img/flatfile-jewel.svg" alt="Flatfile logo" />
