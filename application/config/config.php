88c2c3a8938858b480a4d506f2e490474a11f362
Alnoor Pirani <alnoor@Alnoors-MacBook-Pro.local>
2022-01-01 17:15:05 -0500

    update base_url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8888/ci/';
+// $config['base_url']	= 'http://localhost:8888/ci/';
+$config['base_url']	= 'https://csvjson.com/';
 
 /*
 |--------------------------------------------------------------------------
