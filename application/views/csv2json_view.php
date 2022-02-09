12e44b4959768101520c08d037c08022c6b6c719
Ashley Mulligan <ashleygmulligan@gmail.com>
2022-02-09 13:35:03 -0600

    ui clean up on csv json page

--- application/views/csv2json_view.php
+++ application/views/csv2json_view.php
@@ -1,25 +1,19 @@
 <div class="container px-4 py-5">
 	<div class="row">
 		<div class="description col-md-12">
-			<h1 class="text-primary fw-bold">CSV or TSV > JSON</h1>
+			<h1 class="text-primary fw-bold">CSV or TSV <small class="bi bi-chevron-right"></small>JSON</h1>
 			<p>To get started, upload or paste your data from Excel (saved as CSV or TSV).
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-md-5 more-bottom-margin">
-			<div class="form-group">
-				<label>Upload a CSV file</label>
-				<span class="btn btn-default fileinput-button form-control">
-					<label>
-						<i class="bi bi-upload"></i>
-						<span>Select a file...</span>
-					</label>
-					<input id="fileupload" type="file" name="file" />
-				</span>
+			<div class="mb-5">
+				<label class="form-label">Upload a CSV file</label>
+				<input id="fileupload" type="file" name="file" class="form-control"/>
 			</div>
-			<div class="form-group code-group">
-				<label>Or paste your CSV here</label>
+			<div class="mb-3">
+				<label class="form-label">Or paste your CSV here</label>
 <textarea id="csv" class="form-control input save" rows="18" spellcheck="false">
 album, year, US_peak_chart_post
 The White Stripes, 1999, -
@@ -43,106 +37,130 @@ Nine Miles from the White City, 2013, -
 		</div>
 
 		<div class="col-md-7 more-bottom-margin">
-			<div class="form-group">
-				<label>Options <small>Hover on option for help</small></label>
-				<div class="form-control options">
-					<label class="inline save" title="Choose your separator">
-						Separator
-						<select id="separator" name="separator">
+			<div class="mb-4">
+				<div class="form-check-inline">
+					<label class="form-label me-2 save" title="Choose your separator">
+						<div class="mb-2">Separator</div>
+						<select class="form-select" id="separator" name="separator">
 							<option value="auto" selected="selected">Auto-detect</option>
 							<option value="comma">Comma</option>
 							<option value="semiColon">Semi-colon</option>
 							<option value="tab">Tab</option>
 						</select>
 					</label>
-					&nbsp;
-					<label class="inline" title="Check to parse numbers (i.e. '7e2' would become 700). Uncheck to keep original formatted numbers as strings.">
-						<input type="checkbox" id="parseNumbers" name="parseNumbers" class="save" checked="checked "/> Parse numbers
+					<label class="form-check-label me-2" title="Check to parse numbers (i.e. '7e2' would become 700). Uncheck to keep original formatted numbers as strings.">
+						<input type="checkbox" id="parseNumbers" name="parseNumbers" class="form-check-input save" checked="checked "/> Parse numbers
 					</label>
-					&nbsp;
-					<label class="inline" title="Check to parse potential values as JSON (numbers, null, false, true, [] and {}). Uncheck to keep original values as strings.">
-						<input type="checkbox" id="parseJSON" name="parseJSON" class="save" checked="checked "/> Parse JSON
+					<label class="form-check-label me-2" title="Check to parse potential values as JSON (numbers, null, false, true, [] and {}). Uncheck to keep original values as strings.">
+						<input type="checkbox" id="parseJSON" name="parseJSON" class="form-check-input save" checked="checked "/> Parse JSON
 					</label>
-					&nbsp;
-					<label class="inline" title="Transpose the data beforehand.">
-						<input type="checkbox" id="transpose" name="transpose" class="save" /> Transpose
+					<label class="form-check-label me-2" title="Transpose the data beforehand.">
+						<input type="checkbox" id="transpose" name="transpose" class="form-check-input save" /> Transpose
 					</label>
-					&nbsp;
-					<label class="inline">Output:</label>
-					<label class="radio-inline" title="Output an array of objects."><input type="radio" id="output-array" name="output" class="save" value="array" checked="checked" />Array</label>
-					<label class="radio-inline"  title="Output an object instead of an array. First column is used as hash key."><input type="radio" id="output-hash" name="output" class="save" value="hash" />Hash</label>
-					&nbsp;
-					<label class="inline" title="Minify or compact result by removing spaces and new lines.">
-						<input type="checkbox" id="minify" name="minify" class="save" /> Minify
+					<br/>
+					<label class="form-check-label me-2">Output:</label>
+					<label class="form-check-label me-2" title="Output an array of objects.">
+						<input type="radio" id="output-array" name="output" class="form-check-input save" value="array" checked="checked" />
+						Array
+					</label>
+					<label class="form-check-label me-2" title="Output an object instead of an array. First column is used as hash key.">
+						<input type="radio" id="output-hash" name="output" class="form-check-input save" value="hash" />Hash
+					</label>
+					<label class="form-check-label me-2" title="Minify or compact result by removing spaces and new lines.">
+						<input type="checkbox" id="minify" name="minify" class="form-check-input save" /> Minify
 					</label>
 				</div>
 			</div>
 			<?php $this->load->view('result_textarea_buttons_view', array('result_title' => 'JSON', 'download' => 'csvjson.json')); ?>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-8">
-			<h4>Node.js</h4>
-			<p>
-				This function is available as a <a href="https://www.npmjs.com/package/csvjson-csv2json" target="_blank">npm package</a>.
-			</p>
+</div>
 
-			<h4>Uploading multiple CSVs?</h4>
-			<p>
-				Embed all the functionality of CSVJSON in any web application with <a href="https://flatfile.com/get-started?utm_source=csvjson-tools&utm_medium=header&utm_campaign=q1-2022-csvjson-redesign">Flatfile</a>, and more. Auto-match columns, validate data fields, and provide an intuitive CSV import experience.
-			</p>
 
-			<h4>About CSV</h4>
-			<ul>
-				<li>
-					CSV stands for <a href="http://en.wikipedia.org/wiki/Comma-separated_values" target="_blank">Comma Separated Values</a>.
-					Often used as an interchange data format to represent table records, one per line. CSV is plain text.
-				</li>
-				<li>
-					The CSV format is documented in an RFC memo <a href="https://tools.ietf.org/html/rfc4180" target="_blank">RFC-4180</a>.
-				</li>
-				<li>
-					The first line is often the header, or column names. Each subsequent row is a record and should have the same number of fields.
-				</li>
-				<li>
-					Fields containing the separator character, line breaks and double-quotes must be enclosed inside double quotes <code>"</code>.
-				</li>
-				<li>
-					Other separator are often used like tabs <code>\t</code>or semi-colons <code>;</code>.
-					TSV or Tab Separated Values is used to store table data in Clipboards.
-					When data is copied from Excel for example, it is stored as TSV in the Clipboard.
-				</li>
-				<li>
-					With CSVJSON you can parse values as numbers or JSON. Turn on respective <strong>Parse Numbers</strong> and <strong>Parse JSON</strong> switches to convert valid numbers and JSON (null, false, true, [] and {}).
-				</li>
-				<li>
-					With CSVJSON you can transpose the csv before conversion. Rows become columns, and columns become rows.
-				</li>
-				<li>
-					With CSVJSON you can output a hash (or object) instead of an array. In that case, the hash key will be the first column.
-				</li>
-			</ul>
-			<h4>Change Log</h4>
-			<ul>
-				<li><strong>Mar 20, 2019</strong> Bug fix: Detect duplicate column headers and make them unique. <a href="https://github.com/FlatFilers/csvjson-app/issues/71">GitHub issue #71</a>.</li>
-				<li><strong>Feb 2, 2019</strong> Refactored and published <a href="https://www.npmjs.com/package/csvjson-csv2json" target="_blank">npm package csv2json</a>. Fix for <a href="https://github.com/FlatFilers/csvjson-app/issues/70">GitHub issue #70</a>.</li>
-				<li><strong>Jan 26, 2019</strong> Improvement: Removed 64k limit on download button. Also fixed issue #68 'Cannot work functional with special letters'.</li>
-				<li><strong>Mar 31, 2018</strong> Improvement: Added option to parse JSON values.</li>
-				<li><strong>Dec 18, 2017</strong> Improvement: Added option to minify or compact JSON. <a href="https://github.com/FlatFilers/csvjson-app/issues/21">GitHub issue #21</a></li>
-				<li><strong>Oct 7, 2016</strong> Improvement: Added option to parse number values or not to retain original number formatting. <a href="https://github.com/FlatFilers/csvjson-app/issues/13">GitHub issue #13</a></li>
-				<li><strong>Jul 09, 2016</strong> Fixed bug : If no text is present in a csv field, it was assigned 0 (zero) by default.</li>
-				<li><strong>Jun 20, 2016</strong> Bug fix: strings containing quotes and commas were prematurely cut.</li>
-				<li><strong>Dec 30, 2015</strong> Bug fix: drop quotes on keys of nested objects.</li>
-				<li><strong>Nov 26, 2015</strong> Improvement: Added options to transpose and output object instead of array.</li>
-				<li><strong>Jan 30, 2014</strong> Bug fix: Pasting Excel data into Textarea would cause an upload.</li>
-				<li><strong>Jan 12, 2014</strong> Initial release.</li>
-			</ul>
-		</div>
-		<div class="col-md-4">
-			<?php
-				//$this->load->view('carbonads');
-				//$this->load->view(rand(1,2) == 1 ? "ludo" : "miamboom");
-			?>
-		</div>
-	</div>
+
+			<div class="container px-4 py-5" id="about-flatfile">
+
+			  <h2 class="pb-2 border-bottom">Uploading multiple CSVs?</h2>
+
+				<div class="row row-cols-1 row-cols-sm-1 row-cols-md-12 row-cols-lg-12 g-4 py-5">
+			    <div class="col d-flex align-items-start">
+			      <div>
+							<p>
+								Embed all the functionality of csv<strong>json</strong> in any web application with <a href="https://flatfile.com/get-started?utm_source=csvjson-tools&amp;utm_medium=header&amp;utm_campaign=q1-2022-csvjson-redesign&amp;ajs_event=came_from_csvjson&amp;ajs_prop_ccf_id=b8cdef6a-602c-4993-890c-752924b5ac2a&amp;__hstc=191284213.17efec156b05b5f65379d478482fed10.1642435230343.1643637413336.1644345002104.7&amp;__hssc=191284213.2.1644345002104&amp;__hsfp=668737353">Flatfile</a>. Auto-match columns, validate data fields, and provide an intuitive CSV import experience.
+							</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+<div class="container px-4 py-5" id="about-csvjson">
+
+  <h2 class="pb-2 border-bottom">More Details</h2>
+
+  <div class="row row-cols-1 row-cols-sm-1 row-cols-md-12 row-cols-lg-12 g-4 py-5">
+    <div class="col d-flex align-items-start">
+      <div>
+					<ul>
+						<li>
+							This function is available as a <a href="https://www.npmjs.com/package/csvjson-csv2json" target="_blank">npm package</a>
+						<li>
+							CSV stands for <a href="http://en.wikipedia.org/wiki/Comma-separated_values" target="_blank">Comma Separated Values</a>.
+							Often used as an interchange data format to represent table records, one per line. CSV is plain text.
+						</p>
+						<li>
+							The CSV format is documented in an RFC memo <a href="https://tools.ietf.org/html/rfc4180" target="_blank">RFC-4180</a>.
+						</p>
+						<li>
+							The first line is often the header, or column names. Each subsequent row is a record and should have the same number of fields.
+						</p>
+						<li>
+							Fields containing the separator character, line breaks and double-quotes must be enclosed inside double quotes <code>"</code>.
+						</p>
+						<li>
+							Other separator are often used like tabs <code>\t</code>or semi-colons <code>;</code>.
+							TSV or Tab Separated Values is used to store table data in Clipboards.
+							When data is copied from Excel for example, it is stored as TSV in the Clipboard.
+						</p>
+						<li>
+							With CSVJSON you can parse values as numbers or JSON. Turn on respective <strong>Parse Numbers</strong><p> and <strong>Parse JSON</strong><p> switches to convert valid numbers and JSON (null, false, true, [] and {}).
+						</p>
+						<li>
+							With CSVJSON you can transpose the csv before conversion. Rows become columns, and columns become rows.
+						</p>
+						<li>
+							With CSVJSON you can output a hash (or object) instead of an array. In that case, the hash key will be the first column.
+						</p>
+					</ul>
+				<p>
+					<a class="btn btn-light" data-bs-toggle="collapse" href="#collapseOne" role="button" aria-expanded="false" aria-controls="collapseExample">
+						View Change Log
+					</a>
+				</p>
+      </div>
+    </div>
+
+
+		<div id="collapseOne" class="accordion-collapse collapse showclass row row-cols-1 row-cols-sm-1 row-cols-md-12 row-cols-lg-12 g-4">
+			    <div class="col d-flex align-items-start">
+						<div>
+								<h5>Mar 20, 2019</h5><p> Bug fix: Detect duplicate column headers and make them unique. <a href="https://github.com/FlatFilers/csvjson-app/issues/71">GitHub issue #71</a>.</p>
+								<h5>Feb 2, 2019</h5><p> Refactored and published <a href="https://www.npmjs.com/package/csvjson-csv2json" target="_blank">npm package csv2json</a>. Fix for <a href="https://github.com/FlatFilers/csvjson-app/issues/70">GitHub issue #70</a>.</p>
+								<h5>Jan 26, 2019</h5><p> Improvement: Removed 64k limit on download button. Also fixed issue #68 'Cannot work functional with special letters'.</p>
+								<h5>Mar 31, 2018</h5><p> Improvement: Added option to parse JSON values.</p>
+								<h5>Dec 18, 2017</h5><p> Improvement: Added option to minify or compact JSON. <a href="https://github.com/FlatFilers/csvjson-app/issues/21">GitHub issue #21</a></p>
+								<h5>Oct 7, 2016</h5><p> Improvement: Added option to parse number values or not to retain original number formatting. <a href="https://github.com/FlatFilers/csvjson-app/issues/13">GitHub issue #13</a></p>
+								<h5>Jul 09, 2016</h5><p> Fixed bug : If no text is present in a csv field, it was assigned 0 (zero) by default.</p>
+								<h5>Jun 20, 2016</h5><p> Bug fix: strings containing quotes and commas were prematurely cut.</p>
+								<h5>Dec 30, 2015</h5><p> Bug fix: drop quotes on keys of nested objects.</p>
+								<h5>Nov 26, 2015</h5><p> Improvement: Added options to transpose and output object instead of array.</p>
+								<h5>Jan 30, 2014</h5><p> Bug fix: Pasting Excel data into Textarea would cause an upload.</p>
+								<h5>Jan 12, 2014</h5><p> Initial release.</p>
+						</div>
+
+			      </div>
+			    </div>
+
+  </div>
+
 </div>
--- application/views/result_textarea_buttons_view.php
+++ application/views/result_textarea_buttons_view.php
@@ -1,4 +1,4 @@
-<div class="form-group code-group">
+<div class="form-group code-group mb-3">
   <label><?=$result_title?></label> <span class="result-note"></span>
   <textarea id="result" class="form-control result save" rows="18" spellcheck="false"></textarea>
 </div>
@@ -8,5 +8,3 @@
 <a id="copy" class="btn btn-light" href="#" title="or Ctrl + A then Ctrl + C to copy to clipboard."><i class="bi bi-share"></i> Copy to clipboard</a>
 <a class="btn btn-light" href="#" title="Convert"><i class="bi bi-chevron-right"></i> Convert</a>
 <a class="btn btn-light" href="#" title="Clear"><i class="bi bi-backspace"></i> Clear</a>
-<br/>
-Convert and copy/paste back to your computer or save your result.
